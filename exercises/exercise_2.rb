fd1cc01ed2d1e84ba40c2c28d3426964b6891f8e
Alanna <phan.alanna@gmail.com>
2023-11-17 01:20:53 -0500

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+store1 = @store1 = Store.find_by(id: 1)
+store2 = @store2 = Store.find_by(id: 2)
+puts store1.name
+store1.name = "Missisauga"
+puts store1.name
